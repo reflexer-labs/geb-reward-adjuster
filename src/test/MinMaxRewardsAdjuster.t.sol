d985132b4f957b3c6db33aacbf2564fc05007d18
fabiohild <fabiohildebrand@gmail.com>
2021-08-06 01:19:33 -0300

    WIP unit tests

---
+++ src/test/MinMaxRewardsAdjuster.t.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.6.7;
+
+import "ds-test/test.sol";
+
+contract MinMaxRewardsAdjusterTest is DSTest {
+    function setUp() public {
+
+    }
+
+    function test_sanity() public {
+        assertTrue(true);
+    }
+
+    function testFail_sanity() public {
+        assertTrue(false);
+    }
+}
+
+
+
